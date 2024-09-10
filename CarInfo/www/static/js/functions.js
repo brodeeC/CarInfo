5dc569c2e6166c090771fe42fddadc96eb582c26
brodeeC <brodeeclontz@gmail.com>
2024-09-10 15:38:41 -0400

    light/dark mode

--- CarInfo/www/static/js/functions.js
+++ CarInfo/www/static/js/functions.js
@@ -7,27 +7,3 @@ function displayMenu() {
     }
     
   }
-
-  //Function that makes the site dark based on the users settings.
-  function darkMode() {
-
-    const isDark = window.matchMedia("(prefers-color-scheme:dark)").matches
-  
-    const lightTheme = {
-      backgroundColor : "white",
-      color : "black"
-    }
-  
-    const darkTheme = {
-      backgroundColor : "black",
-      color : "white"
-    }
-  
-    return (
-      <div style={isDark?darkTheme:lightTheme}>
-        Hello World
-      </div>
-    );
-  }
-  
-  export default App;
