4b2460ff13885a22274e634708e5a16342b507fe
kcbailey111 <mrkcbailey@gmail.com>
2024-09-17 15:41:03 -0400

    Updates to Slideshow work

--- CarInfo/www/static/js/functions.js
+++ CarInfo/www/static/js/functions.js
@@ -7,3 +7,16 @@ function displayMenu() {
       x.style.display = "none";
     }
   }
+
+//Code for the Slideshow featured on the Homepage.
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+  .fadeOut(1000)
+  .next()
+  .fadeIn(1000)
+  .end()
+  .appendTo('#slideshow');
+}, 3000);
