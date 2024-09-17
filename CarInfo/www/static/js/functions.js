c696394a49f842c36b55f996bb58c2ab58ae795f
kcbailey111 <mrkcbailey@gmail.com>
2024-09-17 16:01:33 -0400

    Updates to Slideshow
    Things to work on:
    Make sure the next and prev button stay in one place.
    Shrink the size of the images some.
    Fix loading bug when the site first loads images do not appear until the button is clicked.

--- CarInfo/www/static/js/functions.js
+++ CarInfo/www/static/js/functions.js
@@ -8,15 +8,34 @@ function displayMenu() {
     }
   }
 
+
 //Code for the Slideshow featured on the Homepage.
+//Some code sourced from w3 schools. https://www.w3schools.com/howto/howto_js_slideshow.asp
+let slideIndex = 1;
+showSlides(slideIndex);
 
-$("#slideshow > div:gt(0)").hide();
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-setInterval(function() {
-  $('#slideshow > div:first')
-  .fadeOut(1000)
-  .next()
-  .fadeIn(1000)
-  .end()
-  .appendTo('#slideshow');
-}, 3000);
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
