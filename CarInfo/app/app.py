5e345792caa04c988664378e33b475ec7957ceff
brodeeC <brodeeclontz@gmail.com>
2024-09-25 10:50:44 -0400

    groups

--- CarInfo/app/app.py
+++ CarInfo/app/app.py
@@ -25,4 +25,4 @@ def csc_342_groups():
             'Designer': ['Charlie Fink'],
             'Programmer(s)': ['Emirhan Gencer']}}
 
-    return jsonify(groups, status=200, mimetype='application/json')
+    return jsonify(groups)
