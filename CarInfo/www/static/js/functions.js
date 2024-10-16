2ffced4e7d437f2cdf809fc1dc6952f740744cf1
kcbailey111 <mrkcbailey@gmail.com>
2024-10-16 14:41:11 -0400

    Update

--- CarInfo/www/static/js/functions.js
+++ CarInfo/www/static/js/functions.js
@@ -1,13 +1,13 @@
 //Some code sourced from w3 schools. https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
 function displayMenu() {
 
-
     const x = document.getElementById("myLinks");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
+  
   }
 
  
