13b991d7f5263daf7c2016e30567d9a28f9f37ea
kcbailey111 <mrkcbailey@gmail.com>
2024-11-11 10:48:07 -0500

    Updates

--- CarInfo/app/WebScrapping.py
+++ CarInfo/app/WebScrapping.py
@@ -1,2 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+
+def get_match_game_airings(schedule_date):
+    
+    mg_eps_and_times = []
+    r = requests.get('https://www.gameshownetwork.com/schedule'
+                    .format(schedule_date))
+
+    soup = BeautifulSoup(r.text, 'html.parser')
